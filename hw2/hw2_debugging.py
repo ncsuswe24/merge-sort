d03ac424c705356783cc537976872fc44bd37998
lakhanij <164538484+lakhanij@users.noreply.github.com>
2024-09-06 22:07:16 +0000

    Added a pylint code traces and fixed most of the errors

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -1,36 +1,56 @@
 import rand
 
-def mergeSort(arr):
-    if (len(arr) == 1):
+
+def merge_sort(arr):
+    """
+    A recursive implementation of the merge sort algorithm
+
+    Args:
+        arr (list): A list of elements to sort.
+
+    Returns:
+        list: Returns the sorted list of elements.
+    """
+    if len(arr) == 1:
         return arr
 
     half = len(arr)//2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+
+def recombine(left_arr, right_arr):
+    """
+    Recombines the two arrays into a single sorted array.
+
+    Args:
+        left_arr (list): The left half of the elements to sort.
+        right_arr (list): The right half of the elements to sort.
+
+    Returns:
+        list: Returns a single sorted list of elements from the given two args.
+    """
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            right_index += 1
+            merge_arr[left_index + right_index] = right_arr[left_index]
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            left_index += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
 
-    return mergeArr
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
 
-arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
+    return merge_arr
+
+
+new_arr = rand.random_array([None] * 20)
+arr_out = merge_sort(new_arr)
 
 print(arr_out)
-
-
--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,19 @@
 import subprocess
 
+
 def random_array(arr):
+    """
+    This function shuffles an array to randomize it. This allows merge sort to sort it.
+
+    Args:
+        arr (list): A list of elements to shuffle.
+
+    Returns:
+        list: Returns the sorted list of elements.
+    """
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i, _ in enumerate(arr):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
