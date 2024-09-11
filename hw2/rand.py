f1995aba2999bf556273d7796ebdc807756e90fa
Vaibhav Hawaldar <90965132+Vaibhav260@users.noreply.github.com>
2024-09-11 06:04:19 +0530

    Update rand.py - Correct boolean argument for subprocess.check in random_array

--- hw2/rand.py
+++ hw2/rand.py
@@ -15,6 +15,6 @@ def random_array(arr):
     shuffled_num = None
     for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check="False")
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
